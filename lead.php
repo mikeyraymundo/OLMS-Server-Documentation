39a233df0b631f5f9bf7211d735dc553d578ac55
mikeyraymundo <mikeraymundo@gmail.com>
2015-02-25 23:31:21 +0800

    Copied previous documentation

--- lead.php
+++ lead.php
@@ -40,6 +40,31 @@
 
 <?php
 
+/**
+*	This module allows user to update lead details and lead status. It also allows the user
+*	to move on to the next lead after updating the status of the current lead. This module
+*	allows the user to view the lead history of a specific lead to see if its details were
+*	changed
+*
+*	include_once is a control structure that evaluates the specified file during the 
+*	execution of the script
+*
+*	'session_start' is a session function that creates a session or resumes the current 
+*	one based on a session identifier passed via a GET or POST request, or passed via a 
+*	cookie. 
+*	'date_default_timezone_set' is a function that sets the default timezone used by all 
+*	date/time functions in a script 
+*	'extract' is a an array function that allows importing variables into the current 
+*	symbol table from an array
+*
+*	$lead_history_query	checks if there have been changes done to a certain lead
+*	$intent stores the value of whether the user chooses to change the details of a lead or 
+*	update a lead's status
+*	$status_query is the query that allows a status list to be used in the status dropdown
+*
+* 	@var resource
+*/
+
   include_once("db_query.php");
   db_connection();
   // session_details();
